3e2504a4ece0752816e45f035886aa2abc2254a2
sevenneys <sevenneys@gmail.com>
2026-04-21 18:36:38 +0300

    dev--end module save-files

--- modules/arp_scan.py
+++ modules/arp_scan.py
@@ -4,17 +4,21 @@ import sys
 import os
 import platform
 
+
 from modules.write_files import datetime, Files
 
 class ArpScan:
 
     def __init__(self, target: str):
 
+        self.responses = []
+
         self.target = target
         self.arp = ARP(pdst=self.target)
         self.eth = Ether(dst="ff:ff:ff:ff:ff:ff")
         self.packet = self.eth / self.arp
         self.oc = platform.system().lower()
+
 
     def InfoArpScan(self):
 
@@ -31,10 +35,10 @@ class ArpScan:
         print(f"IP | MAC | Vendor | Hostname | Status")
         print("=" * 50)
 
-        
+
     def Scan(self, desc_file=None):
 
-        responses = []
+        is_call = 1
 
         try:
             while True:
@@ -46,21 +50,21 @@ class ArpScan:
                     device_mac = dst.hwsrc
                     check_mac = f"{device_mac}\n"
 
-                    if check_mac not in responses:
+                    if check_mac not in self.responses:
                         print(f"{device_ip} {device_mac} [is ACTIVE 👁️]")
 
-                    responses.append(f"{device_ip} ")
-                    responses.append(f"{device_mac}\n")
-
+                        self.responses.append(f"{device_ip} ")
+                        self.responses.append(f"{device_mac}\n")
+                        
                 time.sleep(6)
 
         except KeyboardInterrupt:
 
             if desc_file != None:
                 new_files = Files()
-                new_files.set_data(responses)
+                new_files.set_data(self.responses)
                 new_files.write_file(desc_file)
 
-            #print()
-            sys.exit
+            print()
+            sys.exit()
 
